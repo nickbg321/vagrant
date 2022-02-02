4218169b833155f85db7f6c0e1b487686effb8fb
Nikolay Ivanov <nikolay.ivanov321@gmail.com>
2022-02-02 19:40:38 +0200

    upgrade PHP to version 8.1

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -7,10 +7,12 @@ export DEBIAN_FRONTEND=noninteractive
 echo "cd /vagrant" >> /home/vagrant/.profile
 
 # Install software
-apt-get update && apt-get install nginx mysql-server \
+add-apt-repository -y ppa:ondrej/php
+apt-update -y
+apt-get install nginx mysql-server \
     redis-server ruby ruby-dev libsqlite3-dev htop build-essential \
-    php7.4-fpm php7.4-mysql php7.4-curl php7.4-cli php7.4-intl php7.4-gd php7.4-zip \
-    php7.4-bcmath php7.4-mbstring php7.4-bz2 php7.4-xml php-xdebug php7.4-soap -y
+    php8.1-fpm php8.1-mysql php8.1-curl php8.1-cli php8.1-intl php8.1-gd php8.1-zip \
+    php8.1-bcmath php8.1-mbstring php8.1-bz2 php8.1-xml php8.1-xdebug php8.1-soap -y
 
 # Install MailCatcher
 gem install mailcatcher
@@ -34,14 +36,14 @@ done
 systemctl restart nginx
 
 # Configure PHP
-cp /vagrant/vagrant/etc/php/php.ini /etc/php/7.4/fpm/conf.d
-cp /vagrant/vagrant/etc/php/php.ini /etc/php/7.4/cli/conf.d
-cp /vagrant/vagrant/etc/php/xdebug.ini /etc/php/7.4/mods-available
+cp /vagrant/vagrant/etc/php/php.ini /etc/php/8.1/fpm/conf.d
+cp /vagrant/vagrant/etc/php/php.ini /etc/php/8.1/cli/conf.d
+cp /vagrant/vagrant/etc/php/xdebug.ini /etc/php/8.1/mods-available
 
-sed -i "s/user = www-data/user = vagrant/g" /etc/php/7.4/fpm/pool.d/www.conf
-sed -i "s/group = www-data/group = vagrant/g" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/user = www-data/user = vagrant/g" /etc/php/8.1/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = vagrant/g" /etc/php/8.1/fpm/pool.d/www.conf
 
-systemctl restart php7.4-fpm
+systemctl restart php8.1-fpm
 
 # Configure MySQL
 mysql -uroot -e "CREATE DATABASE app CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
